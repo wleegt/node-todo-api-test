228fa1a679ba7d9be21ab7c8744cb86b7b287a14
Wook Lee <wleegt@gmail.com>
2016-11-13 23:25:53 +0900

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -21,6 +21,14 @@ app.post('/todos', function(req, res) {
   });
 });
 
+app.get('/todos', function(req, res) {
+  Todo.find().then(function(todos) {
+    res.send({todos});
+  }, function(e) {
+    res.status(400).send(e);
+  });
+});
+
 app.listen(3000, function() {
   console.log('Started on port 3000');
 });
