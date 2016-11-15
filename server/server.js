c504cdff066002fb6ab335edae607b479bf9af7b
Wook Lee <wleegt@gmail.com>
2016-11-15 22:14:52 +0900

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -27,6 +28,24 @@ app.get('/todos', function(req, res) {
   }, function(e) {
     res.status(400).send(e);
   });
+});
+
+app.get('/todos/:id', function(req, res) {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then(function(todo) {
+    if (!todo) {
+      res.status(404).send();
+    }
+    res.send({todo});
+  }).catch(function(e) {
+    res.status(400).send();
+  });
+
 });
 
 app.listen(3000, function() {
